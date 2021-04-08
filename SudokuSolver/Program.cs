dfb0687746d47a19f464e861754f426b15780d35
Sov <spam.reply55@gmail.com>
2021-04-08 14:11:37 +0100

    Added comments in Program.cs referring to the overall plan of the project.

--- SudokuSolver/Program.cs
+++ SudokuSolver/Program.cs
@@ -16,5 +16,13 @@ namespace SudokuSolver {
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
+        //  You can use bruteforce solving for an entire sudoku, but I find this an unelegant solution
+        //  Therefore, I will instead be using a hybrid method of analytical and bruteforce
+        //  The program will attempt to complete the sudoku primarily by analytical means:
+        //  analytical implying the usage of grounded principles
+        //  and bruteforce implying the usage of high computational force to complete the problem.
+        //  If the program fails to complete the sudoku analytically, it will only then resort to bruteforce.
+
     }
 }
