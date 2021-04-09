83496e18e1298b58dda711cf57a700585daea3a7
Sov <spam.reply55@gmail.com>
2021-04-09 12:02:07 +0100

    Added a SudokuSquare.cs struct for storing data about one particular square. A 2D array of these structs can perform the internal representation of the sudoku grid.

---
+++ SudokuSolver/SudokuSquare.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver {
+    // This class is a struct that stores all information about one particular sudoku square
+    // Including coordinates, potential values and real value
+    public struct SudokuSquare {
+        public SudokuSquare(int x, int y) { // x and y are oriented bottom left to top right, 0-based ie 0,0 to 8,8
+            X = x;
+            Y = y;
+            PotentialValues = new List<int> {1,2,3,4,5,6,7,8,9}; // All values are assumed to be possible when grid initialised
+            RealValue = -1; // -1 represents no given value, will be an exception in code using this data
+        }
+
+        public int X, Y;
+        public List<int> PotentialValues;
+        public int RealValue;
+    }
+}
