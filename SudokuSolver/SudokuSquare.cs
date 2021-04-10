2a1e8e2b8290eefc1ae81f63bacfdd8745fb3a2e
Sov <spam.reply55@gmail.com>
2021-04-10 20:54:23 +0100

    Added the SudokuSquareHandler.cs class which handles the management of the SudokuSquare.cs structs; not strictly required but should make management/manipulation a lot easier

--- SudokuSolver/SudokuSquare.cs
+++ SudokuSolver/SudokuSquare.cs
@@ -7,11 +7,12 @@ using System.Threading.Tasks;
 namespace SudokuSolver {
     // This class is a struct that stores all information about one particular sudoku square
     // Including coordinates, potential values and real value
+
     public struct SudokuSquare {
-        public SudokuSquare(int x, int y) { // x and y are oriented bottom left to top right, 0-based ie 0,0 to 8,8
+        public SudokuSquare(int x, int y) { // uses mathematical graph coordinates, not programmatic ones
             X = x;
             Y = y;
-            PotentialValues = new List<int> {1,2,3,4,5,6,7,8,9}; // All values are assumed to be possible when grid initialised
+            PotentialValues = new List<int> {1,2,3,4,5,6,7,8,9};
             RealValue = -1; // -1 represents no given value, will be an exception in code using this data
         }
 
---
+++ SudokuSolver/SudokuSquareHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver {
+    public class SudokuSquareHandler {
+
+        // This list holds all instances of internal representations of sudoku squares
+        public List<SudokuSquare> InternalRepresentation = new List<SudokuSquare>();
+
+        public SudokuSquareHandler() {
+            for (int x = 0; x < 9; x++) {
+                for (int y = 0; y < 9; y++) {
+                    InternalRepresentation.Add(new SudokuSquare(x, y));
+                }
+            }
+        }
+
+        public SudokuSquare GetInternalSquare(int x, int y) {
+            // This returns the SudokuSquare struct of the appropriate coordinates
+            return InternalRepresentation.First(a => a.X == x && a.Y == y);
+        }
+    }
+}
