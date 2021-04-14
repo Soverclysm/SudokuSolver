92cb9f884c4e2f667326901e117c456e8b303597
Sov <spam.reply55@gmail.com>
2021-04-14 15:13:41 +0100

    Created a GraphicsHandler.cs which will handle the transfer of data from graphical TextBoxes to a singular List<SudokuSquare> which can be given to SudokuSquareHandler for solving. It will then also handle the process of showing the solved data to the user. Additionally, created a local arg array full of the graphical TextBoxes. This array will then be passed to GraphicsHandler for it to use.

--- SudokuSolver/Form1.cs
+++ SudokuSolver/Form1.cs
@@ -12,12 +12,18 @@ namespace SudokuSolver {
     public partial class Form1 : Form {
         public SudokuSquareHandler SudokuSquareHandler = new SudokuSquareHandler();
         public BoardAnalyser BoardAnalyser = new BoardAnalyser();
+        public GraphicsHandler GraphicsHandler;
         public Form1() {
             InitializeComponent();
+            TextBox[] arg = {
+                A1, B1, C1, D1, E1, F1, G1, H1,
+                A2, B2, C2, D2, E2, F2, G2, H2,
+                A3, B3, C3, D3, E3, F3, G3, H3,
+            };
         }
 
         private void SolveButtonClicked(object sender, EventArgs e) {
-
+            
         }
     }
 }
